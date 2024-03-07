6812fdda619a28d9fdfc9cf55061ba63b34097c4
ritatanght <doggymint@gmail.com>
2024-03-07 15:24:54 -0500

    Add Card component with basic text props

---
+++ 01-trello-clone/src/Card.tsx
@@ -0,0 +1,11 @@
+import { CardContainer } from "../styles";
+
+interface CardProps {
+  text: string;
+}
+
+const Card = ({ text }: CardProps) => {
+  return <CardContainer>{text}</CardContainer>;
+};
+
+export default Card;
