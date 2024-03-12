a501996ea17f86633bae345f9141a4d379963aff
ritatanght <doggymint@gmail.com>
2024-03-11 22:59:51 -0400

    Wrap context inside withData

--- 01-trello-clone/src/AppStateContext.tsx
+++ 01-trello-clone/src/AppStateContext.tsx
@@ -1,8 +1,10 @@
-import React, { createContext, useReducer, useContext } from "react";
+import React, { createContext, useReducer, useContext, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { findItemIndexById } from "./utils/findItemIndexById";
 import { moveItem } from "./moveItem";
 import { DragItem } from "./DragItem";
+import { save } from "./api";
+import { withData } from "./withData";
 
 interface AppStateContextProps {
   state: AppState;
@@ -29,35 +31,22 @@ export interface AppState {
   draggedItem: DragItem | undefined;
 }
 
-const appData: AppState = {
-  draggedItem: undefined,
-  lists: [
-    {
-      id: "0",
-      text: "To Do",
-      tasks: [{ id: "c0", text: "Generate app scaffold" }],
-    },
-    {
-      id: "1",
-      text: "In Progress",
-      tasks: [{ id: "c2", text: "Learn Typescript" }],
-    },
-    {
-      id: "2",
-      text: "Done",
-      tasks: [{ id: "c3", text: "Begin to use static typing" }],
-    },
-  ],
-};
-
-export const AppStateProvider = ({ children }: React.PropsWithChildren<{}>) => {
-  const [state, dispatch] = useReducer(appStateReducer, appData);
-  return (
-    <AppStateContext.Provider value={{ state, dispatch }}>
-      {children}
-    </AppStateContext.Provider>
-  );
-};
+export const AppStateProvider = withData(
+  ({
+    children,
+    initialState,
+  }: React.PropsWithChildren<{ initialState: AppState }>) => {
+    const [state, dispatch] = useReducer(appStateReducer, initialState);
+    useEffect(() => {
+      save(state);
+    }, [state]);
+    return (
+      <AppStateContext.Provider value={{ state, dispatch }}>
+        {children}
+      </AppStateContext.Provider>
+    );
+  }
+);
 
 export const useAppState = () => {
   return useContext(AppStateContext);
@@ -107,12 +96,10 @@ const appStateReducer = (state: AppState, action: Action): AppState => {
         action.payload;
       const sourceLaneIndex = findItemIndexById(state.lists, sourceColumn);
       const targetLaneIndex = findItemIndexById(state.lists, targetColumn);
-      console.log("sourceColumn", sourceLaneIndex);
-      console.log("targetColumn", targetLaneIndex);
       const item = state.lists[sourceLaneIndex].tasks.splice(dragIndex, 1)[0];
       // insert the task at hoverIndex
       state.lists[targetLaneIndex].tasks.splice(hoverIndex, 0, item);
-      console.log(state);
+
       return { ...state };
     }
     default: {
