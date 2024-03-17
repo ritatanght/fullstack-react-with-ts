b7950a4884211f7bce53787ccc3e4e71a11f22fa
ritatanght <doggymint@gmail.com>
2024-03-16 22:09:04 -0400

    Add Key component

---
+++ 03-react-piano/src/components/Key/Key.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+import React, { FunctionComponent } from "react";
+import {NoteType} from "../../domain/note"
+
+interface KeyProps {
+  type: NoteType;
+  label: string;
+  disabled?: boolean;
+}
+export const Key: FunctionComponent<KeyProps> = (props) => {
+  const { type, label, ...rest } = props;
+  return (
+    <button className={clsx(`key key--${type}`)} type="button" {...rest}>
+      {label}
+    </button>
+  );
+};
